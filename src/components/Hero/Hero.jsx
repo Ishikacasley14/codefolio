3e6e1a3fc3a4f8e4baacccc9be855779e6848ab3
Ishikacasley14 <ishikacasley764@gmail.com>
2024-07-20 23:56:33 +0530

    hero section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,32 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+
 const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <hi className={styles.title}>Hi, I am Ishika</hi>
+        <h1 className={styles.title}>Hi, I am Ishika</h1>
         <p className={styles.description}>
           I am a full stack developer with a passion for building great
           products. I have a strong background
         </p>
-        <a
-          href="mailto:ishikacasley764@gmail.com"
-          className={styles.contactBtn}
-        >
-          Contact Me
-        </a>
+        <div className={styles.buttonContainer}>
+          <a
+            href="mailto:ishikacasley764@gmail.com"
+            className={styles.contactBtn}
+          >
+            Contact Me
+          </a>
+          <a
+            href="/ishikaa.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.resumeBtn}
+          >
+            View My Resume
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
