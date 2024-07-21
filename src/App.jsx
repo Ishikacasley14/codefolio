a712165d8545c904f1f1328c0b1417d5e3139d55
Ishikacasley14 <ishikacasley764@gmail.com>
2024-07-21 23:44:42 +0530

    About section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import style from "./App.module.css";
+import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div className={style.App}>
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Hero image of me"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>ML Engineer</h3>
+              <p>
+                I have experience developing machine learning models and
+                optimizing data.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
